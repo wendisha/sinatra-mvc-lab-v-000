35b892088ba098d128d2d037ac0ade8e75b06364
Wendy Calderon <wendycalderon@gmail.com>
2018-10-18 02:15:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
-  get '/user_input' do
+  get '/' do
     erb :user_input
   end
+  
+  post '/piglatinize' do
+    @translation = PigLatinizer.new.piglatinize(params[:user_phrase])
+    erb :results
+  end 
   
   
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,11 +14,11 @@ class PigLatinizer
         results << x + 'way'
       elsif vowels.include?(x[0])
         results << x + 'way'
-      elsif consonants.include?(x[0]) && consonants.include?(x[1]) && consonants.include?(x[2])
+      elsif consonants.include?(x[0].downcase) && consonants.include?(x[1]) && consonants.include?(x[2])
         results << x[3..-1] + x[0..2] + 'ay'
-      elsif consonants.include?(x[0]) && consonants.include?(x[1])
+      elsif consonants.include?(x[0].downcase) && consonants.include?(x[1])
         results << x[2..-1] + x[0..1] + 'ay'
-      elsif consonants.include?(x[0])
+      elsif consonants.include?(x[0].downcase)
         results << x[1..-1] + x[0] + 'ay'
       else
         results << x # return unchanged
